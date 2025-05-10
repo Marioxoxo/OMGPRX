52a2f491fe42e284bca3f673f6b8435b6c7a200b
Marioxoxo <drsmario10@gmail.com>
2025-05-10 15:11:30 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://marioxoxo-a-caso.hf.space/"
+PASSWORD = "cacca"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
